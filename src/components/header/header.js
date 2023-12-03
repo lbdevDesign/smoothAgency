cf98d0549023b32f885d230bc4c94ea1ad80e234
Lucas <lucasbombaron@gmail.com>
2023-12-03 16:24:49 +0100

    menu + gallery

---
+++ src/components/header/header.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import burger from '../../assets/icon-hamburger.svg'
+
+function Header() {
+    const [showMenu, setMenu] = useState();
+
+    function display() {
+        setMenu(value=>!value)
+      }
+
+    return (
+        <header className="header">
+            <div className="header__logo">smoothAgency</div>
+            <button className="header__burger" onClick={display}>
+                <img src={burger} alt='menu' className='header__burger--icon'/>
+            </button>
+            <div className={`header__menu${showMenu?' active':null}`}>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><button className="contact-button">Contact</button></li>
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
